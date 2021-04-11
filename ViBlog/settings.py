c0d8f4dc0afa3389cb7c6d1d7ac8cf7d0dc65a02
Erick-ViBe <erickvb12@gmail.com>
2021-04-11 18:31:32 -0500

    Added heroku and local host

--- ViBlog/settings.py
+++ ViBlog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['viblogapi.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
